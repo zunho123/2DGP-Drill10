6355788b44835dc4c22b33e18af2b959ed610116
kim junho <zunho8331@tukorea.ac.kr>
2025-11-03 17:04:09 +0900

    새의 크기 조정(크기 정확한 표현)

--- bird.py
+++ bird.py
@@ -17,8 +17,10 @@ class Bird:
     image = None
     FRAME_COLS = 5
     FRAME_ROWS = 3
-    LEFT, RIGHT = 50, 1550
+    LEFT, RIGHT = 20, 1580
     TOP_Y = 540
+    REND_W = 33
+    REND_H = 25
 
     def __init__(self):
         if Bird.image is None:
@@ -28,8 +30,7 @@ class Bird:
         self.frame = 0.0
         self.w = Bird.image.w // self.FRAME_COLS
         self.h = Bird.image.h // self.FRAME_ROWS
-        self.TOTAL_FRAMES = self.FRAME_COLS * self.FRAME_ROWS
-        self.FRAMES_PER_ACTION = self.TOTAL_FRAMES - 1
+        self.FRAMES_PER_ACTION = 14
         self.FLY_RIGHT = FlyRight(self)
         self.FLY_LEFT  = FlyLeft(self)
         self.state_machine = StateMachine(
@@ -60,16 +61,15 @@ class FlyRight:
         if self.b.x >= self.b.RIGHT:
             self.b.x = self.b.RIGHT
             self.b.handle_state_event(('EDGE', None))
-
         self.b.frame = (self.b.frame + self.b.FRAMES_PER_ACTION * ACTION_PER_TIME * dt) % self.b.FRAMES_PER_ACTION
 
     def draw(self):
-        idx = int(self.b.frame)
+        idx = int(self.b.frame) % self.b.FRAMES_PER_ACTION
         col = idx % self.b.FRAME_COLS
         row = idx // self.b.FRAME_COLS
         sy_row = (self.b.FRAME_ROWS - 1 - row)
         sx, sy = col * self.b.w, sy_row * self.b.h
-        Bird.image.clip_draw(sx, sy, self.b.w, self.b.h, self.b.x, self.b.y)
+        Bird.image.clip_draw(sx, sy, self.b.w, self.b.h, self.b.x, self.b.y, self.b.REND_W, self.b.REND_H)
 
 class FlyLeft:
     def __init__(self, bird): self.b = bird
@@ -85,12 +85,12 @@ class FlyLeft:
         self.b.frame = (self.b.frame + self.b.FRAMES_PER_ACTION * ACTION_PER_TIME * dt) % self.b.FRAMES_PER_ACTION
 
     def draw(self):
-        idx = int(self.b.frame)
+        idx = int(self.b.frame) % self.b.FRAMES_PER_ACTION
         col = idx % self.b.FRAME_COLS
         row = idx // self.b.FRAME_COLS
         sy_row = (self.b.FRAME_ROWS - 1 - row)
         sx, sy = col * self.b.w, sy_row * self.b.h
         try:
-            Bird.image.clip_composite_draw(sx, sy, self.b.w, self.b.h, 0, 'h', self.b.x, self.b.y, self.b.w, self.b.h)
+            Bird.image.clip_composite_draw(sx, sy, self.b.w, self.b.h, 0, 'h', self.b.x, self.b.y, self.b.REND_W, self.b.REND_H)
         except:
-            Bird.image.clip_draw(sx, sy, self.b.w, self.b.h, self.b.x, self.b.y)
+            Bird.image.clip_draw(sx, sy, self.b.w, self.b.h, self.b.x, self.b.y, self.b.REND_W, self.b.REND_H)

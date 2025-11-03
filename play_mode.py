43dcff89098c311d3e5a21e86a03f0dcd6fd0cf1
kim junho <zunho8331@tukorea.ac.kr>
2025-11-03 19:30:22 +0900

    겹침 방지를 위해 새의 x좌표에도 랜덤 요소 적용

--- play_mode.py
+++ play_mode.py
@@ -35,7 +35,7 @@ def init():
     x = Bird.LEFT
     top_y = Bird.TOP_Y
 
-    birds = [Bird(x=x, y=top_y - random.randint(0, 150)) for _ in range(10)]
+    birds = [Bird(x=x + random.randint(-5, 5), y=top_y - random.randint(0, 150)) for _ in range(10)]
 
     for b in birds:
         game_world.add_object(b, 2)
